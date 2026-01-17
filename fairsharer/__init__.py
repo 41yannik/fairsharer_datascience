1816a175998f76b72710d524d1e261c18ab7fed9
yannik.huber <yannik_huber@gmx.de>
2026-01-17 13:02:47 +0100

    Init-Datei mit __all__ für Export

--- fairsharer/__init__.py
+++ fairsharer/__init__.py
@@ -1,2 +1,4 @@
-# Macht die fair_sharer Funktion direkt importierbar
 from .fair_sharer import fair_sharer
+
+# __all__ sagt Python, welche Funktionen exportiert werden sollen
+__all__ = ["fair_sharer"]
